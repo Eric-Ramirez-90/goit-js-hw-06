1d1c6f9a49a597c2b54ecd509bf392ec34b6967e
Eric Ramirez <ramirez1990rer@gmail.com>
2022-10-21 18:33:46 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,9 @@
-const countCategories = document.querySelectorAll('.item');
-console.log('amount li.item:', countCategories.length);
+const mainListRef = document.querySelector('#categories');
+console.log('Number of categories:', mainListRef.children.length);
+
+const itemListRef = document.querySelectorAll('.item');
+
+itemListRef.forEach((item) => {
+  console.log('Category:', item.firstElementChild.textContent);
+  console.log('Elements:', item.lastElementChild.children.length);
+});
