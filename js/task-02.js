9a548bfc025f62f949e2ea94bb0e868d85c98ac6
Eric Ramirez <ramirez1990rer@gmail.com>
2022-10-23 14:26:08 +0300

    Completed task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulContainerRef = document.querySelector('#ingredients');
+
+// Перший спосіб
+const markup = ingredients.map((ingredient) => {
+  const createListItem = document.createElement('li');
+  createListItem.classList.add('item');
+  createListItem.textContent = ingredient;
+
+  return createListItem;
+});
+
+ulContainerRef.append(...markup);
+
+// Другий спосіб
+// const arr = [];
+// for (let i = 0; i < ingredients.length; i += 1) {
+
+//   const createListItem = document.createElement('li');
+//   createListItem.classList.add('js-item');
+//   createListItem.textContent = ingredients[i];
+
+//   arr.push(createListItem);
+// }
+// console.log(arr);
+// ulContainerRef.append(...arr);
