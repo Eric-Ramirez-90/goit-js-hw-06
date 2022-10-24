dba047ddd8e17053b57db72a5d021722cc8e1bb4
Eric Ramirez <ramirez1990rer@gmail.com>
2022-10-24 17:27:51 +0300

    Completed task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget;
+
+  if (email.value === '' || password.value.trim() === '') {
+    alert('Please fill in all the fields!');
+  } else {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+
+    event.currentTarget.reset();
+  }
+}
